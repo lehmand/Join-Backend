802162517af5dbd14b21a8f596cb8224542b9a7c
lehmand <daniel.lehmann188@gmail.com>
2025-07-10 21:22:57 +0200

    removed inital data migration

--- join_auth_app/apps.py
+++ join_auth_app/apps.py
@@ -1,27 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
-
-def load_initial_users(sender, **kwargs):
-    """
-    Load initial user data from fixtures after migrations.
-    """
-    from django.core.management import call_command
-
-    from join_auth_app.models import CustomUser
-    if CustomUser.objects.exists():
-        print("Users already exist. Skipping fixture loading.")
-        return
-
-    try:
-        call_command('loaddata', 'initial_users.json')
-        print("Successfully loaded users.json")
-    except Exception as e:
-        print(f"Error loading users.json: {e}")
 
 class JoinAuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'join_auth_app'  # Replace with your app name
+    name = 'join_auth_app'
     verbose_name = 'Join Auth App'
-
-    def ready(self):
-        post_migrate.connect(load_initial_users, sender=self)
