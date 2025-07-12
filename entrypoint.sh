a82fa1fe3f111f9777e61c8069c2202eee9a642b
lehmand <daniel.lehmann188@gmail.com>
2025-07-12 22:01:37 +0200

    pre deploy push

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --noinput
 python create_guest_user.py
-python manage.py runserver 0.0.0.0:8001
+gunicorn --bind 0.0.0.0:8001 join_backend.wsgi:application
